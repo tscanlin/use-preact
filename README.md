30e200534af9acd854485d3dcd7cc4aa7c851ed6
Tim Scanlin <tscanlin@gmail.com>
2017-03-04 18:54:55 -0800

    add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # use-preact
+
 Use preact to reduce bundle sizes, this script will add preact-compat as an alias
+
+
+## Usage
+
+```bash
+npm i --save-dev use-preact
+```
+
+By default use-preact points to the location of the webpack config for create-react-app projects.
+
+```bash
+use-preact <PATH_TO_CONFIG> <SEARCH_STRING>
+
+use-preact ./node_modules/react-scripts/config/ preact-compat
+```
---
+++ use-preact.sh
@@ -0,0 +1,22 @@
+# Use preact
+# Switch a react project to using preact if its isn't using it already.
+#
+# EX: grep -rl "preact-compat" ./node_modules/react-scripts/config/
+# EX: find ./node_modules/react-scripts/config/*.config.* -type f -exec sed -i "s/alias: {/alias: {\n\t\t\t\'react\': \'preact-compat\',\n\t\t\t\'react-dom\': \'preact-compat\',/gi" {} \;
+
+filePattern=${1:-./node_modules/react-scripts/config/}
+searchString=${2:-preact-compat}
+
+replaceString="alias: {"
+newString="alias: {\n\t\t\t\'react\': \'preact-compat\',\n\t\t\t\'react-dom\': \'preact-compat\',"
+
+SEARCH="$(grep -rl "$searchString" $filePattern)"
+echo "${SEARCH}"
+
+if [ ! "${SEARCH}" ]
+  then
+    echo "preact was not detected, adding it as a webpack config alias..."
+
+    REPLACE="$(find $filePattern -type f -exec sed -i "s/$replaceString/$newString/gi" {} \;)"
+    echo "${REPLACE}"
+fi
